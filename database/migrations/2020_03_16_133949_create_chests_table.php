040261757039f62425d91b8d5831a9d3f7138229
Bart <bartlomiej.bartkowski@gmail.com>
2020-03-16 17:29:59 +0100

    Chest - partial

---
+++ database/migrations/2020_03_16_133949_create_chests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('pictureFileName');
+            $table->unsignedFloat('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chests');
+    }
+}
